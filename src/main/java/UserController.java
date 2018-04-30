d4ab0dbed892dfe3af6a1d60e2c87c85eace4bee
gtordjma <gtordjma@student.42.fr>
2018-04-30 03:32:33 +0200

    add User Timeline

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsHelper;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import spark.Request;
 import java.lang.Object;
+import com.github.jknack.handlebars.helper.*;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.SparkConf;
@@ -20,9 +23,32 @@ public class UserController {
 		staticFileLocation("/public");
 		port(4567);
 
+		HandlebarsTemplateEngine handle = new HandlebarsTemplateEngine();
+
 		get("/", (request, response) -> { //request for route happens at this location
-            Map<String, Object> model = model_user_info(request); // new model is made to store information
+			ES es = new ES();
+			Map<String, List<Map<String, Object>>> model = es.get_alltweet();
+			model.put("userinfo", model_user_info(request).get("userinfo"));
 			return new ModelAndView(model, "index.hbs"); // assemble individual pieces and render
+		}, new HandlebarsHelper());
+
+		get("/usertimeline", (request, response) -> { //request for route happens at this location
+			if (getAuthenticatedUser(request) == null)
+				response.redirect("/");
+			ES es = new ES();
+			Map<String, List<Map<String, Object>>> model = es.get_usertimeline(getAuthenticatedUser(request));
+			model.put("userinfo", model_user_info(request).get("userinfo"));
+			return new ModelAndView(model, "index.hbs"); // assemble individual pieces and render
+		}, new HandlebarsHelper());
+
+		post("/", (req, res) -> {
+            ES es = new ES();
+            Map<String, List<Map<String, Object>>> model = model_user_info(req);
+            if (getAuthenticatedUser(req) != null) {
+				Boolean b = es.tweet(req.queryParams("tweet"), getAuthenticatedUser(req));
+			}
+			model.put("tweets", es.get_alltweet().get("tweets"));
+			return new ModelAndView(model, "index.hbs");
 		}, new HandlebarsTemplateEngine());
 
 		get("/signup", (request, response) -> {
@@ -35,13 +61,17 @@ public class UserController {
 		//inscription
 		//curl -d "firstname=Marvin&lastname=Tordjman&email=m@g.co&pwd=toto" -X POST 0.0.0.0:4567/users
 		post("/signup", (req, res) -> {
-			userService.createUser(
+			Result result = userService.signup(
 					req.queryParams("firstname"),
 					req.queryParams("lastname"),
 					req.queryParams("email"),
 					req.queryParams("pwd"));
 			Map<String, Object> model = new HashMap<String, Object>();
-			model.put("res", "Your account has been successfully registered !");
+			if(result.getUser() != null) {
+				model.put("res", "Your account has been successfully registered !");
+			} else {
+				model.put("error", result.getError());
+			}
 			return new ModelAndView(model, "signup.hbs");
 		}, new HandlebarsTemplateEngine()); //
 
@@ -71,37 +101,57 @@ public class UserController {
 		get("/profile", (request, response) -> {
 			if (getAuthenticatedUser(request) == null)
 				response.redirect("/");
-			Map<String, Object> model = model_user_info(request);
+			ES es = new ES();
+			Map<String, List<Map<String, Object>>> model = model_user_info(request);
+			model.put("tweets", es.get_idtweet(Integer.toString(getAuthenticatedUser(request).getId())).get("tweets"));
+			return new ModelAndView(model, "params.hbs");
+		}, new HandlebarsHelper());
+
+		get("/delete/:id", (request, response) -> {
+			if (getAuthenticatedUser(request) == null)
+				response.redirect("/");
+			ES es = new ES();
+			es.DELETE(request.params(":id"));
+			Map<String, Object> model = new HashMap<String, Object>();
+			response.redirect("/");
+			return null;
+		});
+
+		get("/profile/:id", (request, response) -> {/*
+			if (getAuthenticatedUser(request) == null)
+				response.redirect("/");*/
+			ES es = new ES();
+            Map<String, Object> info = new HashMap<String, Object>();
+            List<Map<String, Object>> list = new ArrayList<>();
+			Map<String, List<Map<String, Object>>> model = es.get_idtweet(request.params(":id"));
+			model.put("userinfo", model_user_info(request).get("userinfo"));
+			if (getAuthenticatedUser(request) != null &&
+					getAuthenticatedUser(request).getFollows().contains(Integer.parseInt(request.params(":id")))){
+			    info.put("unfollow", true);
+            }else if (getAuthenticatedUser(request) != null){
+                info.put("follow", true);
+            }
+            list.add(info);
+			model.put("test", list);
 			return new ModelAndView(model, "profile.hbs");
-		}, new HandlebarsTemplateEngine());
+		}, new HandlebarsHelper());
 
-		get("/tweet", (request, response) -> {
-            Map<String, Object> model = model_user_info(request);
-			SparkConf conf = new SparkConf().setAppName("test").setMaster("local");
-			conf.set("spark.driver.allowMultipleContexts", "true");
-			conf.set("es.index.auto.create", "true");
-			conf.set("es.nodes","192.168.0.20");
-			conf.set("es.port","9200");
-			JavaSparkContext jsc = new JavaSparkContext(conf);
+        get("/follow/:id", (req, res) -> {
+            userService.follow(getAuthenticatedUser(req), Integer.parseInt(req.params(":id")));
+            res.redirect("/profile/"+req.params(":id"));
+            return null;
+        });
+        get("/unfollow/:id", (req, res) -> {
+            userService.unfollow(getAuthenticatedUser(req), Integer.parseInt(req.params(":id")));
+            res.redirect("/profile/"+req.params(":id"));
+            return null;
+        });
 
-			JavaRDD<Map<String, Object>> esRDD =
-					JavaEsSpark.esRDD(jsc, "gtordjma/tutoriels", "?q=PHP*").values();
-			List<Map<String, Object>> lst = esRDD.collect();
-			for (int i = 0;i < lst.size();i++){
-				Map<String, Object> map = lst.get(i);
-				for(String key:map.keySet()){
-					System.out.println("treeMap: [key: "+key+" , value: "+map.get(key)+"]");
-				}
-
-			}
-			return new ModelAndView(model, "index.hbs");
-		}, new HandlebarsTemplateEngine());
-
-		get("/alltweet", (request, response) -> {
+/*		get("/alltweet", (request, response) -> {
 			ES es = new ES();
 			Map<String, List<Map<String, Object>>> model = es.get_alltweet();
 			return new ModelAndView(model, "alltweet.hbs");
-		}, new HandlebarsTemplateEngine());
+		}, new HandlebarsTemplateEngine());*/
 
 		get("/logout", (req, res) -> {
 			removeAuthenticatedUser(req);
@@ -113,19 +163,28 @@ public class UserController {
 			res.status(400);
 			res.body(JsonUtil.toJson(new ResponseError(e)));
 		});
+
+		notFound((req, res) -> {
+			res.type("application/json");
+			return "{\"message\":\"Custom 404\"}";
+		});
 	}
 
-	private Map<String, Object> model_user_info(Request request){
-		Map<String, Object> model = new HashMap<String, Object>();
+	private Map<String, List<Map<String, Object>>> model_user_info(Request request){
+		Map<String, List<Map<String, Object>>> model = new HashMap<String, List<Map<String, Object>>>();
+		Map<String, Object> info = new HashMap<String, Object>();
+		List<Map<String, Object>> list = new ArrayList<>();
 		User usr = getAuthenticatedUser(request);
 		if (usr != null) {
-			model.put("id", usr.getId());
-			model.put("firstname", usr.getFirst_name());
-			model.put("lastname", usr.getLast_name());
-			model.put("email", usr.getEmail());
-			model.put("follower", usr.getFollower());
-			model.put("follows", usr.getFollows());
+			info.put("id", usr.getId());
+			info.put("firstname", usr.getFirst_name());
+			info.put("lastname", usr.getLast_name());
+			info.put("email", usr.getEmail());
+			info.put("follower", usr.getFollower());
+			info.put("follows", usr.getFollows());
 		}
+		list.add(info);
+		model.put("userinfo", list);
 		return model;
 	}
 
